c550a847cef9120c0dcaf7ba1d553641c03f5478
asim <asimarunava@gmail.com>
2015-02-02 17:08:58 +0530

    Over ride local

--- src/ChecKex.java
+++ src/ChecKex.java
@@ -12,12 +12,10 @@ public class ChecKex extends IOException{
 	public void doSomething(){
 		System.out.println("hello world");
 	}
-	// Adding the new method
+	
 	public void doSomething_other(){
 		System.out.println("hello world");
 	}
-	//Adding from Computer
-	public void doSomething_other(){
-		System.out.println("hello world");
-	}
+	
+//All Conflict Changes
 }
