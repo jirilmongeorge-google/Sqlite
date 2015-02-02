a2fadae703625b790f1fe7d4140d45ea07b39012
asimarunava <asimarunava@gmail.com>
2015-02-02 16:54:22 +0530

    Update ChecKex.java

--- src/ChecKex.java
+++ src/ChecKex.java
@@ -12,4 +12,8 @@ public class ChecKex extends IOException{
 	public void doSomething(){
 		System.out.println("hello world");
 	}
+	
+	public void doSomething_other(){
+		System.out.println("hello world");
+	}
 }
