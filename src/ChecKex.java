6185feb70eb29c445224dd423d92a0ba55a523f4
asimarunava <asimarunava@gmail.com>
2015-02-03 14:54:26 +0530

    Update ChecKex.java

--- src/ChecKex.java
+++ src/ChecKex.java
@@ -9,22 +9,4 @@ public class ChecKex extends IOException{
 	public void doSomething(){
 		System.out.println("hello world");
 	}
-	
-	public void doSomething_other(){
-		System.out.println("hello world");
-	}
-	
-	public void doSomething_other1(){
-		System.out.println("hello world");
-	}
-	//The method is added later
-	
-	public void doSomething_other2(){
-		System.out.println("hello world");
-	}
-	// see something my changes to be confirmed
-	//see something
-	//yes i can
-	//can i resolve it
-	//can i resolve it
 }
